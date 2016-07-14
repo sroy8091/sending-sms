68bd206199007e278116b3f20928b67aa6d81634
sroy8091 <sroy8091@gmail.com>
2016-07-14 20:26:18 +0530

    Security Reasons

--- send_sms.py
+++ send_sms.py
@@ -1,8 +1,8 @@
 from twilio.rest import TwilioRestClient
  
 # Your Account Sid and Auth Token from twilio.com/user/account
-account_sid = "ACd0f7428305ec431a91adcdec336052a8"
-auth_token  = "e87df40459a730b0fa7dd8df0db1fab9"
+account_sid = "Your sid"
+auth_token  = "Your token"
 client = TwilioRestClient(account_sid, auth_token)
  
 message = client.messages.create(body="Hi partha.",
